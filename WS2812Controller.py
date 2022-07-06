567139be5a7a05455d752934b900782f1a21703a
Bartłomiej Stefański <30830383+bartex00001@users.noreply.github.com>
2022-07-06 13:33:28 +0200

    Created 'active_on_init' option

--- WS2812Controller.py
+++ WS2812Controller.py
@@ -2,11 +2,12 @@ from WS2812 import WS2812
 
 
 class WS2812Controller:
-    def __init__(self, output_pin, num_of_leds, state_machine_id):
+    def __init__(self, output_pin, num_of_leds, state_machine_id, active_on_init=True):
         self.NUM_OF_LEDS = num_of_leds
 
         WS2812Controller.check_sm_id(state_machine_id)
         self.WS2812 = WS2812(output_pin, self.NUM_OF_LEDS, state_machine_id=state_machine_id)
+        self.WS2812.active(active_on_init)
 
     @staticmethod
     def check_sm_id(sm_id):
