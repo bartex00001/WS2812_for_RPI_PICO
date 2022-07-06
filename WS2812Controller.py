88b85a4fd9adc547ec2bff11f7085b27f916550a
Bartłomiej Stefański <30830383+bartex00001@users.noreply.github.com>
2022-07-06 12:29:25 +0200

    Controller creates an instance of WS2812

--- WS2812Controller.py
+++ WS2812Controller.py
@@ -2,4 +2,13 @@ from WS2812 import WS2812
 
 
 class WS2812Controller:
-    pass
+    def __init__(self, output_pin, num_of_leds, state_machine_id):
+        self.NUM_OF_LEDS = num_of_leds
+
+        WS2812Controller.check_sm_id(state_machine_id)
+        self.WS2812 = WS2812(output_pin, self.NUM_OF_LEDS, state_machine_id=state_machine_id)
+
+    @staticmethod
+    def check_sm_id(sm_id):
+        if sm_id < 0 or sm_id > 7:
+            raise ValueError("State machine ID must be between 0 and 7")
